88d6911586e770ee3f97754bb1aabde9c0ba2408
Juan Sebastian Garcia <jsebastiangarcia29@gmail.com>
2023-07-07 12:11:31 -0500

    create-security group

--- main.tf
+++ main.tf
@@ -14,11 +14,38 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = aws_secutiry_group.blog.id
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_secutiry_group" "blog" {
+  name = "log"
+  description = "Allow HTTPS in. Allow Everything out"
+
+  vpc_id = data.aws.default.id
+}
+
+resource = "aws_secutiry_group_rule" "blog_https_in" {
+  
+  type         = "ingress"
+  from_port    = 80
+  to_port      = 80
+  protocol     = "tcp"
+  cidr_blocks  = ["0.0.0.0/0"]
+
+  security_group_id = aws_secutiry_group.blog.id
+
+}
+
+
