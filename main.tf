20d8a5e2a72b26d1bacf801265929c2dc75e4918
Juan Sebastian Garcia <jsebastiangarcia29@gmail.com>
2023-07-07 15:58:36 -0500

    create vpc

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "blog" {
   name = "log"
   description = "Allow HTTPS in. Allow Everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
