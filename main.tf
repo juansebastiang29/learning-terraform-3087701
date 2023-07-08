8bae934404fc352df442c910d1d8b157670071d8
Juan Sebastian Garcia <jsebastiangarcia29@gmail.com>
2023-07-08 14:21:45 -0500

    fix

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -33,53 +33,22 @@ module "vpc" {
 }
 
 
-resource "aws_security_group" "blog" {
-  name = "log"
-  description = "Allow HTTPS in. Allow Everything out"
+# module "blog_autoscaling" {
+#   source  = "terraform-aws-modules/autoscaling/aws"
+#   version = "6.5.2"
 
-  vpc_id = module.vpc.public_subnets[0]
-}
+#   name = "blog"
 
-resource "aws_security_group_rule" "blog_https_in" {
-  
-  type         = "ingress"
-  from_port    = 80
-  to_port      = 80
-  protocol     = "tcp"
-  cidr_blocks  = ["0.0.0.0/0"]
+#   min_size            = 1
+#   max_size            = 2
+#   vpc_zone_identifier = module.blog_vpc.public_subnets
+#   target_group_arns   = module.blog_alb.target_group_arns
+#   security_groups     = [module.blog_sg.security_group_id]
+#   instance_type       = var.instance_type
+#   image_id            = data.aws_ami.app_ami.id
+# }
 
-  security_group_id = aws_security_group.blog.id
-
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name = "blog_new"
-
-  vpc_id = module.vpc.public_subnets[0]
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
-  
-}
-
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
@@ -87,8 +56,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
@@ -97,12 +66,6 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
@@ -119,5 +82,14 @@ module "alb" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
